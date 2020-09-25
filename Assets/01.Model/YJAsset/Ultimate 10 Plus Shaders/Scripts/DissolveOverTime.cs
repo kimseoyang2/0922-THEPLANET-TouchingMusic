955701f4b7dc91a6e727da081124085dc020cb84
LILYSUMMER <64562629+LILYSUMMER@users.noreply.github.com>
2020-09-25 13:37:46 +0900

    파티클 에셋 구매 추가

---
+++ Assets/01.Model/YJAsset/Ultimate 10 Plus Shaders/Scripts/DissolveOverTime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class DissolveOverTime : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    public float speed = .5f;
+
+    private void Start(){
+        meshRenderer = this.GetComponent<MeshRenderer>();
+
+
+    }
+
+    private float t = 1.0f;
+    private void Update(){
+        Reveal();
+    }
+
+    void Reveal()
+    {
+        Material[] mats = meshRenderer.materials;
+
+
+        mats[0].SetFloat("_Cutoff", t);
+
+        t -= speed * Time.deltaTime;
+
+        if (t <= 0)
+            t = 0;
+
+        // Unity does not allow meshRenderer.materials[0]...
+        meshRenderer.materials = mats;
+    }
+}
