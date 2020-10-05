46e3752eda7e4db7c9afc7a5975cb580b2ce2b4c
kimseoyang2 <61591951+kimseoyang2@users.noreply.github.com>
2020-10-05 11:09:17 +0900

    MapUpadate

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -16,8 +16,8 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        StickMove();
-        //ComMove();
+        //StickMove();
+        ComMove();
 
     }
 
