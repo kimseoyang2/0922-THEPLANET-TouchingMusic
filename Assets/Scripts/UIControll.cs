0cf31c1a3c69153baa842c372e35298ed5c817e9
LILYSUMMER <64562629+LILYSUMMER@users.noreply.github.com>
2020-10-26 15:46:54 +0900

    main1026
    UI 작업 중간과정

---
+++ Assets/Scripts/UIControll.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //만약 부딪힌게 UIzone 이라면
+        if (other.CompareTag("UI"))
+        {
+            Debug.Log("UITouch");
+            //UI를 키세요
+            other.transform.GetChild(0).gameObject.SetActive(true);
+
+        };
+        
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("UI"))
+        {
+            Debug.Log("UITouch");
+            //UI를 키세요
+            other.transform.GetChild(0).gameObject.SetActive(false);
+
+        };
+    }
+}
