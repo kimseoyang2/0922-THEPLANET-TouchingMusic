c16e2ef38ac1d75fbe388804b7b5cd0d0b466dd9
LILYSUMMER <64562629+LILYSUMMER@users.noreply.github.com>
2020-10-28 14:43:50 +0900

    1028_all
    이것저것

---
+++ Assets/Scripts/ExampleTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string[] names = Input.GetJoystickNames();
+
+        for(int i = 0; i < names.Length; i++)
+        {
+            if (names[i].Length > 0) print("joystick : " + names[i]);
+        }
+
+        string str = Input.inputString;
+        if(str.Length > 0)
+        {
+            print(str);
+        }
+        
+        if (Input.anyKey)
+            Debug.Log(Input.inputString);
+    }
+}
