faecda4d9ef449419b302fc4504669c88b1ad744
LILYSUMMER <64562629+LILYSUMMER@users.noreply.github.com>
2020-10-29 10:09:57 +0900

    1018_LAST

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private readonly Array keyCodes = Enum.GetValues(typeof(KeyCode));
+
+    void Update()
+    {
+        print("Mouse X :" + Input.GetAxis("Mouse X"));
+        //print("Mouse Y :" + Input.GetAxis("Mouse Y"));
+
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in keyCodes)
+            {
+                if (Input.GetKey(keyCode))
+                {
+                    Debug.Log("KeyCode down: " + keyCode);
+                    break;
+                }
+            }
+        }
+
+    }
+}
