61a250be34eb0864fe1def2db1780e73e7e75443
LILYSUMMER <64562629+LILYSUMMER@users.noreply.github.com>
2020-10-30 11:02:53 +0900

    1030_Soo
    이것저것 ... .

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private readonly Array keyCodes = Enum.GetValues(typeof(KeyCode));
+
+    void Update()
+    {
+       // print("Mouse X :" + Input.GetAxisRaw("Mouse X"));
+        //print("Mouse Y :" + Input.GetAxisRaw("Mouse Y"));
+
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in keyCodes)
+            {
+                if (Input.GetKey(keyCode))
+                {
+                    Debug.Log("KeyCode down: " + keyCode);
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/UISetAtive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISetAtive : MonoBehaviour
+{
+    private float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //시간이 계속 흐른당
+        time += Time.deltaTime;
+        //만약 일정 시간이 지나면 (20초) UI가 켜지도록 한다
+        if(time >= 5f)
+        {
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+        
+    }
+}
