f415b9ed2945268b78e0587216d1b47e45f5c6fa
LILYSUMMER <64562629+LILYSUMMER@users.noreply.github.com>
2020-11-03 16:40:25 +0900

    1103 sy

---
+++ Assets/MyMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MyMove : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
+                Debug.Log("KeyCode down: " + kcode);
+        }
+    }
+}
