25a47c5e65df72ce7eeb8e3703ec1b8609d149c4
LILYSUMMER <64562629+LILYSUMMER@users.noreply.github.com>
2020-11-05 17:07:31 +0900

    112

---
+++ Assets/Scripts/PortalTeleport2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalTeleport2 : MonoBehaviour
+{
+    public int nowSceneNum;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+            SceneManager.LoadScene("Potal");
+
+    }
+}
